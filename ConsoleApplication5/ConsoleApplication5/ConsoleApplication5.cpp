5b88a498e7b7d7fbff1fc04566cc377944c3ddc5
ksiberx <themurt@mail.ru>
2025-09-30 20:32:48 +0900

    Add files via upload
    с++

---
+++ ConsoleApplication5/ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,37 @@
+﻿// 1 Задание
+#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "ru");
+	int a, b, c, d, e;
+
+	cout << "Введите 5 оценок: ";
+	cin >> a >> b >> c >> d >> e;
+
+	switch (a + b + c + d + e >= 20) {
+	case 1: cout << "Допущен"; break;
+	case 0: cout << "Не допущен"; break;
+	}
+	return 0;
+}
+
+// 2 Задание
+#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "ru");
+	int num;
+
+	cout << "Введите число: ";
+	cin >> num;
+
+	switch (num % 2) {
+	case 0:
+		cout << num * 3;
+		break;
+	case 1: 
+		cout << num / 2.0;
+		break;
+	}
+	return 0;
+}
