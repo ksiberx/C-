d99280b4b4aa748a1d7e71408464e68f7227353e
ksiberx <themurt@mail.ru>
2025-09-30 20:17:56 +0900

    Add files via upload
    C++

---
+++ ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int sum = 1 + 2 + 3 + 4 + 5;  
+    cout << "Сумма чисел от 1 до 5: " << sum << endl;
+    return 0;
+}
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "*****" << endl;
+    return 0;
+}
