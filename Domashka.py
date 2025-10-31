3767f2818725b42cecee6ee58819c5fa7197fcda
ksiberx <themurt@mail.ru>
2025-10-31 09:40:09 +0900

    Massivi.cpp

---
+++ Domashka.py
@@ -0,0 +1,74 @@
+// 1 задание
+/*#include <iostream>
+using namespace std;
+int main()
+{
+ setlocale(LC_ALL, "ru");
+ char c = 'A';
+ cout << c;
+ return 0;
+}
+*/
+// 4 задание
+/*
+#include <iostream>
+using namespace std;
+int main()
+{
+ setlocale(LC_ALL, "ru");
+ int num;
+
+ cout << "Введите число: ";
+ cin >> num;
+
+ if (num % 2 == 0)
+  cout << "Число четное!" << endl;
+ else
+  cout << "Число нечётное!" << endl;
+ return 0;
+}
+*/
+
+// 7 задание
+/*
+#include <iostream>
+using namespace std;
+int main()
+{
+ setlocale(LC_ALL, "ru");
+ for (int i = 1; i <= 10; i++) {
+  cout << i << "^3 = " << i * i * i << endl;
+}
+return 0;
+ 
+}
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+ setlocale(LC_ALL, "ru");
+
+
+
+
+}
+
+// 13 задание 
+/*#include <iostream>
+using namespace std;
+int main()
+{
+ setlocale(LC_ALL, "ru");
+ int num = 1;
+
+ for (int i = 0; i < 3; i++) {
+  for (int j = 0; j < 3; j++) {
+   cout << num << "";
+   num++;
+  }
+  cout << endl;
+ }
+ return 0;
+}
+*/
