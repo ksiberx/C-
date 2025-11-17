b02f17904133a57f3d34dc65ae81fab594a4eefb
ksiberx <themurt@mail.ru>
2025-11-17 20:04:28 +0900

    Add files via upload

--- Domashka/ConsoleApplication17.cpp
+++ Domashka/ConsoleApplication17.cpp
@@ -1,59 +1,76 @@
 ﻿#include <iostream>
 using namespace std;
 
-// Задание 1
-void printRect(int N, int K) {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < K; j++) cout << "*";
-        cout << endl;
+// ЗАДАНИЕ 1
+double power(double a, int n) {
+    double r = 1;
+    for (int i = 0; i < n; i++) r *= a;
+    return r;
+}
+
+//  ЗАДАНИЕ 2 
+int sumRange(int a, int b) {
+    int s = 0;
+    for (int i = a; i <= b; i++) s += i;
+    return s;
+}
+
+//  ЗАДАНИЕ 3 
+bool isPerfect(int x) {
+    int s = 0;
+    for (int i = 1; i <= x / 2; i++)
+        if (x % i == 0) s += i;
+    return s == x;
+}
+
+//  ЗАДАНИЕ 4 
+void printCard(char suit, int value) {
+    cout << "Карта: ";
+    switch (value) {
+    case 6: cout << "6 "; break;
+    case 7: cout << "7 "; break;
+    case 8: cout << "8 "; break;
+    case 9: cout << "9 "; break;
+    case 10: cout << "10 "; break;
+    case 11: cout << "Валет "; break;
+    case 12: cout << "Дама "; break;
+    case 13: cout << "Король "; break;
+    case 14: cout << "Туз "; break;
+    }
+    switch (suit) {
+    case 'C': cout << "♣️"; break;
+    case 'D': cout << "♦️"; break;
+    case 'H': cout << "♥️"; break;
+    case 'S': cout << "♠️"; break;
     }
 }
 
-
-// Задание 2
-int factorial(int n) {
-    int f = 1;
-    for (int i = 1; i <= n; i++) f *= i;
-    return f;
-}
-
-
-// Задание 3
-bool isPrime(int n) {
-    if (n < 2) return false;
-    for (int i = 2; i * i <= n; i++)
-        if (n % i == 0) return false;
-    return true;
-}
-
-
-// Задание 4
-int cube(int x) {
-    return x * x * x;
-}
-
-
-// Задание 5
-int max2(int a, int b) {
-    return (a > b) ? a : b;
-}
-
-
-// Задание 6
-bool isPositive(int x) {
-    return x > 0;
+//  ЗАДАНИЕ 5 
+bool isLuckyHex(int x) {
+    int a = x / 16;
+    int b = x % 16;
+    return a == b;
 }
 
 int main() {
     setlocale(LC_ALL, "ru");
 
-    // Примеры использования (можно убрать)
-    printRect(3, 5);
-    cout << factorial(5) << endl;
-    cout << (isPrime(7) ? "простое" : "не простое") << endl;
-    cout << cube(3) << endl;
-    cout << max2(8, 4) << endl;
-    cout << (isPositive(5) ? "true" : "false") << endl;
+    //  Проверка заданий 
+
+    cout << "Задание 1: " << power(2, 5) << endl;
+
+    cout << "Задание 2: " << sumRange(1, 5) << endl;
+
+    cout << "Задание 3: ";
+    for (int i = 1; i <= 10000; i++)
+        if (isPerfect(i)) cout << i << " ";
+    cout << endl;
+
+    cout << "Задание 4: ";
+    printCard('H', 12);
+    cout << endl;
+
+    cout << "Задание 5: " << (isLuckyHex(0x33) ? "счастливое" : "несчастливое") << endl;
 
     return 0;
 }
