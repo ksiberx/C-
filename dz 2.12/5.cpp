b0868f8c4e7bf0a3b05b46d5dc562a29b249ab2a
ksiberx <themurt@mail.ru>
2025-11-30 22:35:01 +0900

    Add files via upload

---
+++ dz 2.12/5.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>           
+#include <cmath>              
+using namespace std;
+
+double phi(double x) {        // функция 
+    return 0.4 * (1 - sin(x));  // уравнение
+}
+
+int main() {
+    double x = 1.1;           
+    double eps = 1e-6;        // точность 10^-6
+    double xn;                
+
+    do {
+        xn = phi(x);           // считаем новое x
+        x = xn;                 // обновляем x
+    } while (fabs(phi(x) - x) > eps);  // пока разница > eps
+
+    cout << "x = " << x;   // вывод
+    return 0;
+}
