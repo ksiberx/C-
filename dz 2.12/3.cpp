81cad1f254db2ca49c9a49fbf3369f44f5e1feb8
ksiberx <themurt@mail.ru>
2025-11-30 22:33:18 +0900

    Add files via upload

---
+++ dz 2.12/3.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>               
+#include <cmath>                  
+using namespace std;
+
+double f(double x) {              // функция
+    return sin(x) - x;            // уравнение
+}
+
+int main() {
+    double a = 1.1, b = 1.2;      // переменные
+    double eps = 1e-4;            
+    int N = (int)(log((b - a) / eps) / log(2)) + 1;      
+
+    double c;                     
+    for (int i = 0; i < N; i++) { // делаем N шагов
+        c = (a + b) / 2;          
+        if (f(a) * f(c) <= 0)    
+            b = c;                
+        else
+            a = c;                
+    }
+
+    cout << "x = " << c;          // выводим корень
+    return 0;
+}
