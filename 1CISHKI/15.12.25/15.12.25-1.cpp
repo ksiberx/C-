cabc278e9e546244d68f30804e5eb143eddc717f
ksiberx <themurt@mail.ru>
2025-12-05 22:46:45 +0900

    Create 15.12.25-1.cpp

---
+++ 1CISHKI/15.12.25/15.12.25-1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+bool inArray(int x, int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == x) return true;
+    }
+    return false;
+}
+
+void task1() {
+    int M, N;
+    cout << "Введите M и N: ";
+    cin >> M >> N;
+
+    int* A = new int[M];
+    int* B = new int[N];
+
+    cout << "Введите массив A: ";
+    for (int i = 0; i < M; i++) cin >> A[i];
+
+    cout << "Введите массив B: ";
+    for (int i = 0; i < N; i++) cin >> B[i];
+
+    int* temp = new int[M];
+    int k = 0;
+
+    for (int i = 0; i < M; i++) {
+        if (!inArray(A[i], B, N) && !inArray(A[i], temp, k)) {
+            temp[k++] = A[i];
+        }
+    }
+
+    int* C = new int[k];
+    for (int i = 0; i < k; i++) C[i] = temp[i];
+
+    cout << "Результат: ";
+    for (int i = 0; i < k; i++) cout << C[i] << " ";
+
+    delete[] A;
+    delete[] B;
+    delete[] temp;
+    delete[] C;
+}
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    task1();
+    return 0;
+}
