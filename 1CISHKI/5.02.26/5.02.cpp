84637cac1551734ee7357330280003ff0784d79e
ksiberx <themurt@mail.ru>
2026-01-26 18:10:45 +0900

    Create 5.02.cpp

---
+++ 1CISHKI/5.02.26/5.02.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string s;
+int pos = 0;
+
+// Функции объявляем заранее
+double expression();
+double term();
+double factor();
+
+// Программа
+int main() {
+    setlocale(LC_ALL, "ru");
+    cout << "Введите выражение: ";
+    getline(cin, s);
+
+    double result = expression();
+
+    cout << "Результат: " << result << endl;
+
+    return 0;
+}
+
+// Разбор + и -
+double expression() {
+    double value = term();
+
+    while (pos < s.length()) {
+        if (s[pos] == '+') {
+            pos++;
+            value += term();
+        }
+        else if (s[pos] == '-') {
+            pos++;
+            value -= term();
+        }
+        else break;
+    }
+
+    return value;
+}
+
+// Разбор * и / 
+double term() {
+    double value = factor();
+
+    while (pos < s.length()) {
+        if (s[pos] == '*') {
+            pos++;
+            value *= factor();
+        }
+        else if (s[pos] == '/') {
+            pos++;
+            value /= factor();
+        }
+        else break;
+    }
+
+    return value;
+}
+
+double factor() {
+    if (s[pos] == '(') {
+        pos++;     
+        double value = expression();
+        pos++;
+        return value;
+    }
+
+    double number = 0;
+
+    while (pos < s.length() && isdigit(s[pos])) {
+        number = number * 10 + (s[pos] - '0');
+        pos++;
+    }
+
+    return number;
+}
