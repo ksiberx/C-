d5346dfd0543168400b901cbc2dd74c3a07d83e5
ksiberx <themurt@mail.ru>
2026-01-28 20:40:45 +0900

    Create 2.02-3.kt

---
+++ 3RAZRABOTKA/2.02/2.02-3.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val hexNumber: Int = 0xFF
+    val binaryNumber: Int = 0b1010
+    val longNumber: Long = 100_000L
+    val floatNumber: Float = 3.14f
+
+    println(hexNumber)
+    println(binaryNumber)
+    println(longNumber)
+    println(floatNumber)
+}
