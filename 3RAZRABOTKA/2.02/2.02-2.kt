41671137fd4384b8416548db5df77b64a36e92b2
ksiberx <themurt@mail.ru>
2026-01-28 20:40:25 +0900

    Create 2.02-2.kt

---
+++ 3RAZRABOTKA/2.02/2.02-2.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val name: String = "Муртузали"
+    var age: Int = 18
+
+    println("Меня зовут $name, мне $age лет.")
+
+    age += 1
+    println("Меня зовут $name, мне $age лет.")
+}
