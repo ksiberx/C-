62bec2af882273396c2a4b61e3b4f81c3bdd5fce
ksiberx <themurt@mail.ru>
2026-05-09 12:08:15 +0900

    Create 3.cs

---
+++ 8C#/10.05-2/3.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите A: ");
+            int A = int.Parse(Console.ReadLine());
+            Console.Write("Введите B: ");
+            int B = int.Parse(Console.ReadLine());
+
+            for (int i = A; i <= B; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
